fae34f7143ce6ae3dee1fdf710abc54aaf0d68b8
Samson Wong <samson9993@gmail.com>
2018-01-17 20:20:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,13 +17,16 @@ function logWhisper(string){
 var uppercase, lowercase 
 
 function sayHiToGrandma(string){
- if(string.toLowerCase() === string) {
-     console.log("I can\'t hear you!")
- }else if(string.toUpperCase === string()){
-    console.log("YES INDEED!")}}}
-    else (string ==='I love you, Grandma.'){
-    console.log('I love you, too.');
-
+    if (string.toLowerCase() === string){
+      return 'I can\'t hear you!';
+    }
+    if (string.toUpperCase() === string){
+      return 'YES INDEED!';
+    }
+    if (string === 'I love you, Grandma.'){
+      return 'I love you, too.';
+    }
+}
 
 
 
